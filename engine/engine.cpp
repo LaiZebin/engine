efe855dfae62adfc50206c850cece61851eb5681
zebin <zebin@192.168.1.103>
2016-03-04 01:32:08 +0800

    FirstTest

---
+++ engine/engine.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : engine.cpp
+// Author      : LaiZebin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	bool noramlRuing = true;
+	//bind socket
+	//listen
+	while(noramlRuing)
+	{
+		//onTimer
+		//netWork
+		//gobige collect
+	}
+	return EXIT_SUCCESS;
+}
